456e2985ef394a069dc236a23867fd32f7f94576
Eva Garcia <emgg2222@gmail.com>
2020-12-21 17:18:36 +0100

    feat: organizar grupos y los partidos de cada grupo

--- classes/Group.js
+++ classes/Group.js
@@ -1,8 +1,91 @@
+const LOCAL_TEAM = 0
+const AWAY_TEAM = 1
 export default class Group {
-    constructor(name, teams)
+    constructor(name, teams )
     {
         this.name = name
         this.teams = teams
+        this.matches = []
+        
+    }
+
+
+    initSchedule(round) {
+        const numberOfMatchDays = this.teams.length - 1
+        const numberOfMatchesPerMatchDay = this.teams.length / 2
+        for (let i = 0; i < numberOfMatchDays; i++) {
+            const matchDay = []  // jornada vacía
+            for (let j = 0; j < numberOfMatchesPerMatchDay; j++) {
+                const match = ['Equipo local', 'Equipo visitante']  // partido
+                matchDay.push(match)
+            }
+            // una vez añadidos todos los partidos a la jornada
+            round.push(matchDay)  // añadimos la jornada a la planificación
+        }
+    }
+
+    setLocalTeams(round) {
+        const maxHomeTeams = this.teams.length - 2
+        let teamIndex = 0
+        round.forEach(matchDay => { // por cada jornada
+            matchDay.forEach(match => { // por cada partido de cada jornada
+                // establecer el equipo local
+                match[LOCAL_TEAM] = this.teams[teamIndex]
+                teamIndex++
+                if (teamIndex > maxHomeTeams) {
+                    teamIndex = 0
+                }
+            })
+        })
+    }
+
+    setAwayTeams(round) {
+        const maxAwayTeams = this.teams.length - 2
+        let teamIndex = maxAwayTeams
+        round.forEach(matchDay => {
+            let firstMatchFound = false
+            matchDay.forEach(match => {
+                if (!firstMatchFound) {
+                    firstMatchFound = true
+                } else {
+                    match[AWAY_TEAM] = this.teams[teamIndex]
+                    teamIndex--
+                    if (teamIndex < 0) {
+                        teamIndex = maxAwayTeams
+                    }
+                }
+            })
+        })
+    }
+
+    fixLastTeamSchedule(round) {
+        let matchDayNumber = 1
+        const lastTeamName = this.teams[this.teams.length - 1]
+        round.forEach(matchDay => {
+            const firstMatch = matchDay[0]
+            if (matchDayNumber % 2 == 0) { // si jornada par -> juega en casa
+                firstMatch[AWAY_TEAM] = firstMatch[LOCAL_TEAM]
+                firstMatch[LOCAL_TEAM] = lastTeamName
+            } else { // jornada impar -> juega fuera
+                firstMatch[AWAY_TEAM] = lastTeamName
+            }
+            matchDayNumber++
+        })
+    }
+
+
+    scheduleMatches()
+    {
+
+        const newRound = []
+        this.initSchedule(newRound)
+        this.setLocalTeams(newRound)
+        this.setAwayTeams(newRound)
+        this.fixLastTeamSchedule(newRound)
+
+        this.matches = newRound
+    
+        
     }
     
 }
--- classes/League.js
+++ classes/League.js
@@ -1,4 +1,5 @@
 import Utilities from '../utils.js'
+import Group from './Group.js'
 /*import Group from './css/Group.js' */
 
 const LOCAL_TEAM = 0
@@ -37,166 +38,44 @@ export default class League {
         }
     }
 
-    initSchedule(round) {
-        const numberOfMatchDays = this.teams.length - 1
-        const numberOfMatchesPerMatchDay = this.teams.length / 2
-        for (let i = 0; i < numberOfMatchDays; i++) {
-            const matchDay = []  // jornada vacía
-            for (let j = 0; j < numberOfMatchesPerMatchDay; j++) {
-                const match = ['Equipo local', 'Equipo visitante']  // partido
-                matchDay.push(match)
-            }
-            // una vez añadidos todos los partidos a la jornada
-            round.push(matchDay)  // añadimos la jornada a la planificación
-        }
-    }
-
-    getTeamNames() {
-        return this.teams.map(team => team.name)
-    }
-    
-    getTeamNamesForSchedule() {
-        const teamNames = this.getTeamNames()
-        if (teamNames.length % 2 == 0) { // son pares
-            return teamNames
-        } else {
-            return [...teamNames, null]
-        }
-    }
-
-    setLocalTeams(round) {
-        const teamNames = this.getTeamNamesForSchedule()
-        const maxHomeTeams = teamNames.length - 2
-        let teamIndex = 0
-        round.forEach(matchDay => { // por cada jornada
-            matchDay.forEach(match => { // por cada partido de cada jornada
-                // establecer el equipo local
-                match[LOCAL_TEAM] = teamNames[teamIndex]
-                teamIndex++
-                if (teamIndex > maxHomeTeams) {
-                    teamIndex = 0
-                }
-            })
-        })
-        /* Este código sería el equivalente al superior usando bucles clásicos
-        for (let i = 0; i < this.matchDaySchedule.length; i++) {
-            const matchDay = this.matchDaySchedule[i]
-            for (let j = 0; j < matchDay.length; j++) {
-                const match = matchDay[j]
-                // establecer equipo local
-            }
-        }
-        */
-    }
-
-    setAwayTeams(round) {
-        const teamNames = this.getTeamNamesForSchedule()
-        const maxAwayTeams = teamNames.length - 2
-        let teamIndex = maxAwayTeams
-        round.forEach(matchDay => {
-            let firstMatchFound = false
-            matchDay.forEach(match => {
-                if (!firstMatchFound) {
-                    firstMatchFound = true
-                } else {
-                    match[AWAY_TEAM] = teamNames[teamIndex]
-                    teamIndex--
-                    if (teamIndex < 0) {
-                        teamIndex = maxAwayTeams
-                    }
-                }
-            })
-        })
-    }
-
-    fixLastTeamSchedule(round) {
-        let matchDayNumber = 1
-        const teamNames = this.getTeamNamesForSchedule()
-        const lastTeamName = teamNames[teamNames.length - 1]
-        round.forEach(matchDay => {
-            const firstMatch = matchDay[0]
-            if (matchDayNumber % 2 == 0) { // si jornada par -> juega en casa
-                firstMatch[AWAY_TEAM] = firstMatch[LOCAL_TEAM]
-                firstMatch[LOCAL_TEAM] = lastTeamName
-            } else { // jornada impar -> juega fuera
-                firstMatch[AWAY_TEAM] = lastTeamName
-            }
-            matchDayNumber++
-        })
-    }
-
-
-    scheduleMatchDays() {
-        for (let i = 0; i < this.config.rounds; i++) {
-            const newRound = this.createRound()
-            // si la jornada es par, invertir partidos
-            if (i % 2 != 0) {
-                for (const matchDay of newRound) {
-                    for (const match of matchDay) {
-                        const localTEam = match[LOCAL_TEAM]
-                        match[LOCAL_TEAM] = match[AWAY_TEAM]
-                        match[AWAY_TEAM] = localTEam
-                    }
-                }
-            }
-            this.matchDaySchedule = this.matchDaySchedule.concat(newRound)
-        }
-    }
-
-    createRound() {
-        // https://es.wikipedia.org/wiki/Sistema_de_todos_contra_todos
-        const newRound = []
-        this.initSchedule(newRound)
-        this.setLocalTeams(newRound)
-        this.setAwayTeams(newRound)
-        this.fixLastTeamSchedule(newRound)
-        return newRound
-    }
-
-    scheduleMatchDays2() {
-        const newRound = this.createRound()
-        const i = 1
-        this.matchDaySchedule = this.matchDaySchedule.concat(newRound)
-        const secondRound = this.matchDaySchedule.map(matchDay => {
-            return matchDay.map(match => {
-                const newMatch = [...match]
-                if (i % 2 != 0) {
-                    const localTEam = newMatch[LOCAL_TEAM]
-                    newMatch[LOCAL_TEAM] = newMatch[AWAY_TEAM]
-                    newMatch[AWAY_TEAM] = localTEam
-                }
-                return newMatch
-            })
-        })
-        this.matchDaySchedule = this.matchDaySchedule.concat(secondRound)
-    }
 
 
     // Mi parte
 
     organizeGroups()  {
        
-       
-
-        console.log("numero equipos", this.teams.length)
-        console.log("numero grupos", this.config.groups)
         const numTeamPerGroup = this.teams.length / this.config.groups
+        let teamsPerGroup = []
+        const groups = []
         let indexTeam = 0
         let numTeamSaveInGroup = 1
         let letterAscii = 65; //La letra del grupo empieza en A
-        console.log("grupo",String.fromCharCode(letterAscii))
-      /*   this.teams.forEach(team => {
-            if(numTeamSaveInGroup == 1)
-            {
-                const groupName = 
+        
+        this.teams.forEach(team => {
+            teamsPerGroup.push (team);  
+            if(numTeamSaveInGroup == numTeamPerGroup)
+            { 
+               
+                console.log ( "GRUPO ", String.fromCharCode(letterAscii))
+                console.log("equipos", teamsPerGroup)  
+                
+                const group = new Group("Grupo "+String.fromCharCode(letterAscii), teamsPerGroup)
+                group.scheduleMatches()
+                groups.push(group) 
+                
+                teamsPerGroup = []           
+                letterAscii++
+                numTeamSaveInGroup = 1
+            } else 
+             {           
+                      
+                numTeamSaveInGroup++
+             }
+            
+            
 
-            }
-            if(numTeamSaveInGroup < numTeamPerGroup)
-            {
-                const newGroup  
-            }
-            numTeamSaveInGroup++
-
-        })        */
+        })    
+     
+        console.log("grupos", groups);   
     }
 }
